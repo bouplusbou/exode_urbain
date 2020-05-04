60dde638c35d06e2ab238a78971929da2894b58d
Baptiste Boucher <baptiste.boucher@gmail.com>
2020-05-04 09:21:56 +0200

    first push

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="h-screen" style="background-color: #FBDDB7">
+      <header class="py-4 px-12 flex justify-between items-end">
+        <p
+          class="text-2xl"
+          style="color: #023F29; font-family: 'Titillium Web'"
+        >
+          EXODE URBAIN
+        </p>
+        <ul class="flex">
+          <li
+            class="cursor-pointer"
+            style="font-family: 'Roboto'; font-weight: bold; padding-right: 60px; color: #2B6451"
+          >
+            Trouve ton job
+          </li>
+          <li
+            class="cursor-pointer"
+            style="font-family: 'Roboto'; font-weight: bold; padding-right: 60px; color: #2B6451"
+          >
+            Découvre la newsletter
+          </li>
+          <li
+            class="cursor-pointer"
+            style="font-family: 'Roboto'; font-weight: bold; padding-right: 60px; color: #2B6451"
+          >
+            Pourquoi partir
+          </li>
+          <li
+            class="cursor-pointer"
+            style="font-family: 'Roboto'; font-weight: bold; color: #2B6451"
+          >
+            Où partir
+          </li>
+        </ul>
+      </header>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
